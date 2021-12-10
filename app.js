740991a09564fe4e71dca20112e650d2e27396da
Pablo Acosta <pabloacobeh@gmail.com>
2021-12-10 10:33:12 -0500

    starting

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+// const fielUpload = require("express-fileupload");
+const app = express();
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connected to DB"))
+  .catch(() => console.log("Couldn't connect to db"));
+
+app.use(express.json());
+app.use(cors());
+// app.use(
+//   fileUpload({
+//     useTempFiles: true,
+//     tempFileDir: "/tmp/",
+//     createParentPath: true,
+//   })
+// );
+
+app.get("/", async (req, res) => {
+  res.json({ message: "Route works" });
+});
+
+// app.use("/api/v1/products", require("./routes/product"));
+// app.use("/api/v1/auth", require("./routes/auth"));
+
+const port = process.env.PORT;
+app.listen(port, () => console.log("Server running..."));
